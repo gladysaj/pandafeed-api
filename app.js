0e06f8ba94894c107acff856c551bac2f54364d2
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-07-02 20:07:17 -0500

    config: cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Dotenv config
 require("dotenv").config();
 
 const express = require('express');
@@ -5,10 +6,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// Config cors
+const cors = require("cors");
 
-// Configure mongoose
+// Config mongoose
 const mongoose = require("mongoose");
 
 mongoose
@@ -25,13 +26,22 @@ mongoose
 
 const app = express();
 
+app.use(
+    cors({
+        origin: ["http://localhost:3001"],
+        credentials: true,
+    })
+ );
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
