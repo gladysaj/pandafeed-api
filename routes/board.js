8c841fd4ec014b12a177cafe5f7c0ad19d421eec
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-07-18 18:22:24 -0500

    feat: add get user boards

--- routes/board.js
+++ routes/board.js
@@ -5,6 +5,21 @@ const Board = require("../models/Board");
 const Company = require("../models/Company");
 const { veryToken } = require("../utils/auth");
 
+// Get my boards
+router.get("/get-boards", veryToken, async (req, res) => {
+  const { user } = req;
+
+  try {
+    const company = await Company.findOne({ companyName: user.company });
+    const boards = await Board.find({ company: company._id });
+    console.log(boards);
+    res.status(200).json(boards);
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error);
+  }
+})
+
 // Create board
 router.post("/create-board", veryToken, async (req, res) => {
   const { user } = req;
