7a708a8045f2ae66f69858126509d80bc9a3be27
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-07-18 22:25:44 -0500

    feat: add extra property to api response

--- routes/board.js
+++ routes/board.js
@@ -5,6 +5,25 @@ const Board = require("../models/Board");
 const Company = require("../models/Company");
 const { veryToken } = require("../utils/auth");
 
+// Get single board
+router.get("/get-board/:companyName/:boardName", async (req, res) => {
+  const { companyName, boardName } = req.params;
+
+  try {
+    const company = await Company.findOne({ companyName });
+    const boards = await Board.find({ company: company._id });
+    const board = boards.filter(board => board.title === boardName)[0];
+    if (!board) {
+      res.status(404).json('Board not found!');
+    } else {
+      res.status(200).json(board)
+    }
+  } catch(error) {
+    console.log(error)
+    res.status(400).json(error);
+  }
+});
+
 // Get my boards
 router.get("/get-boards", veryToken, async (req, res) => {
   const { user } = req;
@@ -12,8 +31,9 @@ router.get("/get-boards", veryToken, async (req, res) => {
   try {
     const company = await Company.findOne({ companyName: user.company });
     const boards = await Board.find({ company: company._id });
-    console.log(boards);
-    res.status(200).json(boards);
+    const objectsArray = boards.map(doc => doc.toObject())
+    const updated = objectsArray.map(obj => ({ ...obj, companyName: user.company }))
+    res.status(200).json(updated);
   } catch (error) {
     console.log(error)
     res.status(400).json(error);
